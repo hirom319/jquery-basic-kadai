cfd07fb78c55dfb869d167e0b2555026d14e38cc
hirom319 <hmiu319@gmail.com>
2023-06-04 10:57:26 +0900

    js files updated

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -2,3 +2,11 @@ $('#change-color').on('click', function() {
     $('#target').css('color', 'blue');
 });
 
+$('#change-text').on('click', function() {
+    $('#target').text("Hello!");
+});
+
+$('#fade-out').on('click', function() {
+    $('#target').fadeOut();
+});
+
